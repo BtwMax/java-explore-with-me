1c3da023148c1a20f85895729703a410d79f27d7
Max <maks.popoff2009@yandex.ru>
2023-09-19 22:52:20 +0500

    feat: 2 часть, основной сервис.

--- stats/stats-service/src/main/java/ru/practicum/stats/statsservice/controller/StatsController.java
+++ stats/stats-service/src/main/java/ru/practicum/stats/statsservice/controller/StatsController.java
@@ -2,10 +2,12 @@ package ru.practicum.stats.statsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 import ru.practicum.ewmservice.stats.statsdto.InnerStatsDto;
 import ru.practicum.ewmservice.stats.statsdto.OutHitsDto;
+import ru.practicum.stats.statsservice.exception.BadRequestException;
 import ru.practicum.stats.statsservice.service.StatsService;
 
 import javax.validation.Valid;
@@ -25,6 +27,7 @@ public class StatsController {
     }
 
     @PostMapping(path = "/hit")
+    @ResponseStatus(HttpStatus.CREATED)
     public void addStats(@RequestBody @Valid InnerStatsDto innerStatsDto) {
         log.info("Запрос на добавление статистики посещения ивента");
         statsService.addStats(innerStatsDto);
@@ -38,6 +41,9 @@ public class StatsController {
                                      @RequestParam(required = false) List<String> uris,
                                      @RequestParam(defaultValue = "false") boolean unique) {
         log.info("Запрос вывод статистики посещений ивента");
+        if (end.isBefore(start)) {
+            throw new BadRequestException("Неверные временные рамки");
+        }
         return statsService.getStats(start, end, uris, unique);
     }
 }
